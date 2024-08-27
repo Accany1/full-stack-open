67de45b235b53e9073d8f22466c8120e3cb58d48
Accany1 <129660499+Accany1@users.noreply.github.com>
2024-08-27 16:16:54 +0800

    commit 4.18

--- part4/controllers/blog.js
+++ part4/controllers/blog.js
@@ -2,6 +2,7 @@ const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+
 blogRouter.get('/', async (request, response) => {
     Blog
       .find({}).populate('user', { username: 1, name: 1, id:1 })
